331bfe93bfe0e87f5159f0c716713eacb83ae808
Alexei Znamensky <russoz@cpan.org>
2011-05-09 13:46:31 -0300

    Using @TestingMania, added twitter options, added POD
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/Dist/Zilla/PluginBundle/Author/RUSSOZ.pm
+++ lib/Dist/Zilla/PluginBundle/Author/RUSSOZ.pm
@@ -13,6 +13,28 @@ use Dist::Zilla 4.102341;    # dzil authordeps
 
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
+has twitter => (
+    is      => 'ro',
+    isa     => 'Bool',
+    lazy    => 1,
+    default => sub {
+        ( defined $_[0]->payload->{no_twitter}
+              and $_[0]->payload->{no_twitter} == 1 ) ? 0 : 1;
+    },
+);
+
+has twitter_tags => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub {
+        my $default = q{#cpan #perl};
+        return $default . defined $_[0]->payload->{twitter_tags}
+          ? q{ } . $_[0]->payload->{twitter_tags}
+          : '';
+    },
+);
+
 sub configure {
     my $self = shift;
 
@@ -35,22 +57,14 @@ sub configure {
         'AutoPrereqs',
 
         'ReportVersions::Tiny',
-        'CompileTests',
-        'EOLTests',
-        'PodCoverageTests',
-        'UnusedVarsTests',
-        'CriticTests',
-        'HasVersionTests',
-        'KwaliteeTests',
-        'MetaTests',
-        'PodSyntaxTests',
-        'NoTabsTests',
     );
+
+    $self->add_bundle('TestingMania');
 
     $self->add_plugins(
         [
             'Twitter' => {
-                hash_tags => '#perl #cpan',
+                hash_tags => $self->twitter_tags,
                 tweet_url =>
                   q(http://search.cpan.org/~{{$AUTHOR_LC}}/{{$DIST}}),
                 url_shortener => 'TinyURL',
@@ -63,3 +77,71 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+	# in dist.ini
+	[@Author::RUSSOZ]
+
+=head1 DESCRIPTION
+
+C<Dist::Zilla::PluginBundle::Author::RUSSOZ> provides shorthand for
+a L<Dist::Zilla> configuration approximately like:
+
+	[@Basic]
+
+	[MetaJSON]
+	[ReadmeFromPod]
+	[InstallGuide]
+	[GitFmtChanges]
+	max_age    = 365
+	tag_regexp = ^.*$
+	file_name  = Changes
+	log_format = short
+
+	[OurPkgVersion]
+	[PodWeaver]
+	[AutoPrereqs]
+
+	[ReportVersions::Tiny]
+	[@TestingMania]
+
+	[Twitter]
+	tweet_url = http://search.cpan.org/~{{$AUTHOR_LC}}/{{$DIST}}
+	hash_tags = #perl #cpan
+	url_shortener = TinyURL
+
+=head1 USAGE
+
+Just put C<[@Author::DOHERTY]> in your F<dist.ini>. You can supply the following
+options:
+
+=over 4
+
+=item *
+
+C<no_twitter> says that releases of this module shouldn't be tweeted.
+
+=item *
+
+C<twitter_tags> says which B<additional> hash tags will be used in the
+release tweet. The tags C<#cpan> and C<#perl> are always added.
+
+=back
+
+
+=head1 SEE ALSO
+
+C<L<Dist::Zilla>>
+
+=head1 ACKNOWLEDGMENTS
+
+Much of the first implementation was shamelessly copied from
+L<Dist::Zilla::PluginBundle::Author::DOHERTY>.
+
+=cut
+
